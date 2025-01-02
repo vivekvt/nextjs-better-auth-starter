4ab01db1c6d1e7933e0499b0f28ca3eae36471ae
jabirdev <adm.jabirdev@gmail.com>
2025-01-02 23:02:26 +0700

    feat: Setup Better Auth with Drizzle and Supabase #2

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,6 +1,13 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import SignOutButton from "../(auth)/components/button-signout";
+import { getMe } from "@/actions/user";
 
-export default function Home() {
+export default async function Home() {
+  const me = await getMe();
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
@@ -48,6 +55,20 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        {me ? (
+          <div className="flex w-full flex-col gap-5">
+            <h2>Hi, {me.name}</h2>
+            <p>{me.email}</p>
+            <SignOutButton />
+          </div>
+        ) : (
+          <Link
+            href={"/signin"}
+            className={cn(buttonVariants({ variant: "default" }))}
+          >
+            Sign In
+          </Link>
+        )}
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
         <a
