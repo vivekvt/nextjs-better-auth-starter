6bff39a4f6dcc8becf430ed3758c6ea88d40e1fb
cakfan <taufan.fhm8@gmail.com>
2025-01-21 11:49:15 +0700

    build: fix dynamic server error

--- src/actions/user/me.ts
+++ src/actions/user/me.ts
@@ -7,17 +7,12 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 
 export async function getMe(): Promise<UserType | null | undefined> {
-  try {
-    const session = await auth.api.getSession({ headers: await headers() });
-    if (!session) {
-      return null;
-    }
-
-    return await db.query.user.findFirst({
-      where: eq(user.id, session.user.id),
-    });
-  } catch (error) {
-    console.error("GET_ME:", error);
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) {
     return null;
   }
+
+  return await db.query.user.findFirst({
+    where: eq(user.id, session.user.id),
+  });
 }
