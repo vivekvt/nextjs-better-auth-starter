84ea18eb54d9d8c38c511c1962759bb79b4bad2a
cakfan <taufan.fhm8@gmail.com>
2025-03-26 14:53:56 +0700

    fix: Swap DATABASE_URL and DIRECT_URL for correct pooling behavior #3

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 const client = postgres(
   process.env.NODE_ENV === "production"
-    ? process.env.DIRECT_URL!
-    : process.env.DATABASE_URL!,
+    ? process.env.DATABASE_URL! // Use DATABASE_URL in production for pooling
+    : process.env.DIRECT_URL!, // Use DIRECT_URL in development for direct access
 );
 export const db = drizzle(client, { schema });
