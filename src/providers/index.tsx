030173f9f013f97d07205c7400477551dd3418ed
cakfan <taufan.fhm8@gmail.com>
2025-07-10 13:13:05 +0700

    fix: nextjs-toploader color

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,11 +4,7 @@ import NextTopLoader from "nextjs-toploader";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <NextTopLoader
-        easing="ease"
-        showSpinner={false}
-        color="hsl(var(--primary))"
-      />
+      <NextTopLoader easing="ease" showSpinner={false} color="var(--primary)" />
       {children}
       <Toaster position="top-center" />
     </>
