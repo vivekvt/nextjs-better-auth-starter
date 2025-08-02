300dbc2128a1322791f5a1c01f7e55016f8bd4ed
cakfan <taufan.fhm8@gmail.com>
2025-08-02 22:59:08 +0700

    chore: remove custom schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 
-const schemaName = process.env.DB_SCHEMA_NAME ?? "public";
-
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
-  schemaFilter: [schemaName],
   dbCredentials: {
     url: process.env.DIRECT_URL!,
   },
